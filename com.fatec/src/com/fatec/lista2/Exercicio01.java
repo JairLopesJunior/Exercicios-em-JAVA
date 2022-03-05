2f629612b85a73116711dc9067ae70fd2a16d2fe
unknown <jair.lopes@fatec.sp.gov.br>
2022-03-04 22:33:29 -0300

    Adicionado a Lista 2 de exercicios e efetuado a resolução do Primeiro exercicio

---
+++ com.fatec/src/com/fatec/lista2/Exercicio01.java
@@ -0,0 +1,24 @@
+package com.fatec.lista2;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+	static Scanner scan;
+	
+	// 1) Fazer um programa que gere a saída.
+	// * * * * * * * * * * * *
+	// O,2,4,6,8,10,12,14
+	// * * * * * * * * * * * *
+
+	public static void main(String[] args) {
+		scan = new Scanner(System.in);
+		System.out.println("Informe um número: ");
+		Integer num = scan.nextInt();
+		for(int i = 0; i <= num; i++) {
+			if(i % 2 == 0) {
+				System.out.printf("%o, ", i);
+			}
+		}
+	}
+
+}
