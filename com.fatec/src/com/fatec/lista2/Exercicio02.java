64c33eaf45dee3656b144c3c0fecc138c249dd38
unknown <jair.lopes@fatec.sp.gov.br>
2022-03-04 23:05:10 -0300

    Adicionado a resolução do Exercicio 2

---
+++ com.fatec/src/com/fatec/lista2/Exercicio02.java
@@ -0,0 +1,38 @@
+package com.fatec.lista2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercicio02 {
+	static Scanner scan;
+	
+	// Preencher um vetor com números inteiros (10 unidades); solicitar um número do teclado.
+	// Pesquisar se esse número existe no vetor. Se existir, imprimir em qual posição do vetor esse
+	// número está. Se não existir, imprimir alguma mensagem que não existe.
+
+	public static void main(String[] args) {		
+		// Criando o VETOR de 10 posições
+		int[] numeros = new int[10];
+		
+		// Instanciando a Classe Random
+		Random gerador = new Random();
+		
+		// Gerando números aleátorios de 0 a 999
+		for(int i = 0; i < numeros.length; i++) {
+			numeros[i] = gerador.nextInt(1000);
+			System.out.println(numeros[i]);
+		}
+		
+		scan = new Scanner(System.in);
+		System.out.println("Informe um número: ");
+		int num = scan.nextInt();
+		for(int j = 0; j < numeros.length; j++) {
+			if(numeros[j] == num) {
+				System.out.printf("O número %o está na posição %o do VETOR!!", num, j);
+				return;
+			}
+		}
+		System.out.printf("Número %o não existe no VETOR!!", num);
+	}
+
+}
